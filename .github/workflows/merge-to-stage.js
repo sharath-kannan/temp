e6573ecc65f661fdcb10184d21effccb9c898d04
sharathkannan <138484653+sharath-kannan@users.noreply.github.com>
2024-07-12 14:17:17 +0530

    Update merge-to-stage.js

--- .github/workflows/merge-to-stage.js
+++ .github/workflows/merge-to-stage.js
@@ -1,13 +1,46 @@
 const STAGE = 'main';
+
+const getChecks = ({ pr, github, owner, repo }) =>
+  github.rest.checks
+    .listForRef({ owner, repo, ref: pr.head.sha })
+    .then(({ data }) => {
+      const checksByName = data.check_runs.reduce((map, check) => {
+        if (
+          !map.has(check.name) ||
+          new Date(map.get(check.name).completed_at) <
+          new Date(check.completed_at)
+        ) {
+          map.set(check.name, check);
+        }
+        return map;
+      }, new Map());
+      pr.checks = Array.from(checksByName.values());
+      return pr;
+    });
+const getReviews = ({ pr, github, owner, repo }) =>
+  github.rest.pulls
+    .listReviews({
+      owner,
+      repo,
+      pull_number: pr.number,
+    })
+    .then(({ data }) => {
+      pr.reviews = data;
+      return pr;
+    });
 const main = async (params) => {
   github = params.github;
   owner = params.context.repo.owner;
   repo = params.context.repo.repo;
-  console.log("owner",owner);
-  console.log("repo",repo);
+  console.log("owner", owner);
+  console.log("repo", repo);
   let prs = await github.rest.pulls
     .list({ owner, repo, state: 'open', per_page: 100, base: STAGE })
     .then(({ data }) => data);
+  await Promise.all([
+    ...prs.map((pr) => getChecks({ pr, github, owner, repo })),
+    ...prs.map((pr) => getReviews({ pr, github, owner, repo })),
+  ]);
   console.log(prs);
 };
 
